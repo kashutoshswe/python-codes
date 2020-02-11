f29f4fd0de007be608cddbd32e08ba2f9bedb031
Ashutosh Kumar <ashutoshkumardbms@gmail.com>
2020-02-11 17:42:24 +0530

    Join Book and Review - Formatted Output

---
+++ Mongo-Example-Implementation/Books/joinBookReviewFormatted.py
@@ -0,0 +1,28 @@
+''' 
+Author : Ashutosh Kumar
+Version : 1.0
+Description : Join books and Reviews documents/Collection
+Email : ashutoshkumardbms@gmail.com
+'''
+
+import pymongo
+import datetime
+
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+mydb = myclient['booklibrary']
+mycol = mydb['reviews']
+
+result = [{'$lookup': {
+    "from": "books",
+    "localField": "book_id",
+    "foreignField": "book_id",
+    "as": "book_comments"
+}},
+    {
+        '$project':
+            {"reviewer_name": 1, 'rating': 1,'review_text':1}
+    }
+]
+
+for doc in mycol.aggregate(result):
+    print(doc)
